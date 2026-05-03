688f0a5d8ae9336c8fbbffc47df4928cb3476b28
Seakorv <arttudun@gmail.com>
2026-05-03 23:31:08 +0300

    Added date picker for date picking

--- akamon-tehtava/src/App.tsx
+++ akamon-tehtava/src/App.tsx
@@ -1,20 +1,38 @@
 import { useState, useEffect } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 //import axios from 'axios';
 // TODO: MUI date picker jossain välissä
 
 function App() {
-  const [startDate, setStartDate] = useState(Date);
-  const [endDate, setEndDate] = useState(Date);
+  const [startDate, setStartDate] = useState<Dayjs | null>(null);
+  const [endDate, setEndDate] = useState<Dayjs | null>(null);
 
   return (
-    <div>
-      <h1>{startDate}</h1>
-      <input
-        type='startDate'
-        value={startDate}
-        onChange={(e) => setStartDate(e.target.value)}
-      />
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="App">
+        <h2>Start Date</h2>
+        <DatePicker 
+          value={startDate}
+          onChange={(newDate) => setStartDate(newDate)}
+          format="DD/MM/YYY"
+        />
+        <h2>End Date</h2>
+        <DatePicker
+          minDate={startDate ?? undefined}
+          value={endDate}
+          onChange={(newDate) => setEndDate(newDate)}
+          format="DD/MM/YYY"
+        />
+        <h1>Displaying dates</h1>
+        <p>Start date is {startDate?.format('DD.MM.YYYY') || 'None'}</p>
+        <p>End date is {endDate?.format('DD.MM.YYYY') || 'None'}</p>
+      </div>
+    </LocalizationProvider>
   );
 }
 
