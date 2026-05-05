2c5645e05df7810d9d0c65f15ac42abec9516f24
Arttu Juntunen <73191683+Seakorv@users.noreply.github.com>
2026-05-05 03:08:05 +0300

    Update convertPrice.ts
    Edited comments from snt to c to be consistent with the language and other instances of the c/kWh

--- akamon-tehtava/src/utils/convertPrice.ts
+++ akamon-tehtava/src/utils/convertPrice.ts
@@ -1,9 +1,9 @@
 const VAT_MULTIPLIER = 1.255;
 
 /**
- * Converts the price from EUR/mWh without VAT to snt/kWh with VAT
+ * Converts the price from EUR/mWh without VAT to c/kWh with VAT
  * @param priceEURMWH the price in EUR/mWh without VAT
- * @returns the price in snt/kWh with VAT added
+ * @returns the price in c/kWh with VAT added
  */
 export const convertPrice = (priceEURMWH: number) => {
   return priceEURMWH * 0.1 * VAT_MULTIPLIER
